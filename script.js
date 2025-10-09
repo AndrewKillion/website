73c1a2c0811aff1f57e39cb5234fd0dce1f09d07
Andrew Killion <andrew@saikat.us>
2025-10-09 18:38:54 -0400

    added js

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+    });
+    document.getElementById('timeDisplay').textContent = timeString;
+}
+updateTime();
+setInterval(updateTime, 1000);
